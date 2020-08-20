c6730d2a339ac9f8b6cbfecb7a01e7e71bc213ba
Mihai Slujitoru <mihai@sodawebmedia.com>
2020-08-20 15:21:34 -0400

    use custom hook with reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
+import endpoint from './endpoint';
 
 import './styles.scss';
+// import { forEach } from 'lodash';
+
+const intialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    };
+  }
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+
+  return state;
+};
+
+const useFetch = url => {
+  const [state, dispatch] = useReducer(fetchReducer, intialState);
+
+  useEffect(() => {
+    dispatch({ type: 'LOADING' });
+
+    const fetchUrl = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        dispatch({ type: 'RESPONSE_COMPLETE', payload: { response: data } });
+      } catch (error) {
+        dispatch({ type: 'ERROR', payload: { error } });
+      }
+    };
+
+    fetchUrl();
+
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     setLoading(false);
+    //     setResponse(response);
+    //   })
+    //   .catch(error => {
+    //     setLoading(false);
+    //     setError(error);
+    //   });
+  }, []);
+
+  return [state.result, state.loading, state.error];
+};
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const characters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,7 +87,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
